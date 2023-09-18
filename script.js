86e6f8045dd8bf26b38e0ac72215466eb87468c0
Sandra Katsikis <mindfulripples@gmail.com>
2023-09-18 17:56:56 +1000

    add eventListeners to chooseMovie button

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// let data;
+const chooseMovieButton = document.getElementById("chooseMovie");    
+const menu = document.getElementById("menu");
+
+chooseMovieButton.addEventListener("mouseenter", () => {chooseMovieButton.style.backgroundColor='gold';})
+chooseMovieButton.addEventListener("mouseleave", () => {chooseMovieButton.style.backgroundColor='white';})
+
+
+chooseMovieButton.addEventListener("click", () => {
+    if (menu.classList.contains("menu_active")) {
+        menu.classList.remove("menu_active");
+    } else {
+        menu.classList.add("menu_active");
+    }
+});
+
+
+
+
+
 async function fetchData(path) {
     const rootURL = "https://swapi.dev/api/";
     const response = await fetch(rootURL + path);
