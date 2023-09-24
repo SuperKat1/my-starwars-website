7d709bf5daf4717cdc47126ab079d6d289a8e83f
Sandra Katsikis <mindfulripples@gmail.com>
2023-09-24 19:16:08 +1000

    import data from SWAPI and generate next level of links

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// let data;
+
 const chooseMovieButton = document.getElementById("chooseMovie");    
 const menu = document.getElementById("menu");
+const lists = document.getElementById("lists");
+const characters = document.getElementById("characters");
+const planets = document.getElementById("planets");
+const starships = document.getElementById("starships");
 
 chooseMovieButton.addEventListener("click", () => {
     if (menu.classList.contains("menu_active")) {
@@ -10,25 +14,59 @@ chooseMovieButton.addEventListener("click", () => {
     }
 });
 
-async function fetchData(path) {
+async function fetchData() {
     const rootURL = "https://swapi.dev/api/";
-    const response = await fetch(rootURL + path);
-    const data = await response.json();
+    const responseFilm = await fetch(rootURL + "films");
+    const filmData = await responseFilm.json();
+
+    const responsePeople = await fetch(rootURL + "people");
+    const peopleData = await responsePeople.json();
+
+    const responsePlanets = await fetch(rootURL + "planets");
+    const planetData = await responsePlanets.json();
+
+    const responseStarships = await fetch(rootURL + "starships");
+    const starshipData = await responseStarships.json();
+
     chooseMovieButton.removeAttribute("disabled");  // this is required because the API response time is really, really slow
     chooseMovieButton.style.backgroundColor='white';  // this indicates to the webpage user that the menu select is ready to use
     chooseMovieButton.addEventListener("mouseenter", () => {chooseMovieButton.style.backgroundColor='gold';});
     chooseMovieButton.addEventListener("mouseleave", () => {chooseMovieButton.style.backgroundColor='white';});
 
-    data.results.forEach(film => {
+    filmData.results.forEach(film => {
         const filmButton = document.createElement('button')
         filmButton.innerText = film.title
-        menu.append(filmButton)
-    });
-    
-}  
-fetchData("films")
 
-// Drop down films menu
+        filmButton.addEventListener("click", () => {
+            lists.classList.add("lists_active");
+            characters.innerHTML = "";
+            planets.innerHTML = "";
+            starships.innerHTML = "";
+            film.characters.forEach((c) => {
+                const a = document.createElement('a');
+                a.setAttribute("href", c);
+                a.innerText = c;
+                characters.append(a);
+            });
+            film.planets.forEach((c) => {
+                const a = document.createElement('a');
+                a.setAttribute("href", c);
+                a.innerText = c;
+                planets.append(a);
+            });
+            film.starships.forEach((c) => {
+                const a = document.createElement('a');
+                a.setAttribute("href", c);
+                a.innerText = c;
+                starships.append(a);
+            });
+        });
+        menu.append(filmButton)
+    });  
+}  
+fetchData()
+
+
 
     
 
